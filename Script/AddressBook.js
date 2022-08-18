054a0be64d1d20a28cd7cd0e8c0c8840193968cd
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-08-18 14:33:22 +0530

    Added: Remaining feild has been added

---
+++ Script/AddressBook.js
@@ -0,0 +1,21 @@
+const text = document.querySelector("#text");
+const textError = document.querySelector(".text-error");
+text.addEventListener("input",function(){
+    let RegexName=RegExp("^[A-Z]{1}[a-z]{3,}$");
+    if(RegexName.test(text.value)){
+        textError.textContent = "";
+    }else{
+        textError.textContent = "Invalid name";
+    }
+});
+
+const number = document.querySelector("#phoneNumber");
+const telError = document.querySelector(".tel-error");
+number.addEventListener("input",function(){
+    let RegexTel=RegExp("^[1-9]{2}\\s{0,1}[0-9]{10}$");
+    if(RegexTel.test(phoneNumber.value)){
+        telError.textContent = "";
+    }else{
+        telError.textContent = "Invalid phone number";
+    }
+});
